0d17d37b6faa34a3c572657b0ed9d34e39e3c82d
Herime <contact@herime.com>
2025-10-27 03:22:22 +0100

    Fix: Corriger les erreurs variables dans admin/users et messages

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -128,7 +128,7 @@
                                     <div class="list-group-item border-0 py-3">
                                         <div class="row align-items-center">
                                             <div class="col-md-2">
-                                                <img src="{{ $message->receiver->avatar ? $instructor->avatar : 'https://ui-avatars.com/api/?name=' . urlencode($message->receiver->name) . '&background=003366&color=fff' }}" 
+                                                <img src="{{ $message->receiver->avatar ? $message->receiver->avatar : 'https://ui-avatars.com/api/?name=' . urlencode($message->receiver->name) . '&background=003366&color=fff' }}" 
                                                      alt="{{ $message->receiver->name }}" class="rounded-circle" width="40" height="40">
                                             </div>
                                             <div class="col-md-6">
